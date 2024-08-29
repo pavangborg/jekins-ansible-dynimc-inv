025097417094c1525d639bf2056862bb02503301
pavan-devgb <pavan24680@gmail.com>
2024-08-29 23:33:37 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,11 +1,11 @@
 provider "aws"{
-region = "ap-southeast-1"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "AWSInstance"{
-  ami = "ami-0d07675d294f17973"
+  ami = "ami-02b49a24cfb95941c"
   instance_type = "t2.micro"
-  key_name = "devopsmssjune2024"
+  key_name = "devopsjune2024"
   security_groups = ["launch-wizard-2"]
   tags={
   Name = "terraformserver"
